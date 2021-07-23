e82d69a0e16d8838904df82b4fae94332e1cce44
João Paiva <joao.vitor.vbp@gmail.com>
2021-07-23 16:26:20 -0300

    Esqueleto dos metodos obrigatorios

--- Gerenciador-de-Salas/src/GerenciadorDeSalas.java
+++ Gerenciador-de-Salas/src/GerenciadorDeSalas.java
@@ -1,3 +1,27 @@
 public class GerenciadorDeSalas {
+    public void adicionaSalaChamada(String nome, int capacidadeMaxima, String descricao) {
+        
+    }
+    public void removeSalaChamada(String nomeDaSala){
+
+    }
+    public List<Sala> listaDeSalas(){
+
+    }
+    public void adicionaSala(Sala novaSala){
+
+    }
+    public Reserva reservaSalaChamada(String nomeDaSala, LocalDateTime dataInicial, LocalDateTime dataFinal){
+
+    }
+    public void cancelaReserva(Reserva cancelada){
+
+    }
+    public Collection<Reserva> reservasParaSala(String nomeSala){
+
+    }
+    public void imprimeReservasDaSala(String nomeSala){
+
+    }
 
 }
--- Gerenciador-de-Salas/src/MarcadorDeReuniao.java
+++ Gerenciador-de-Salas/src/MarcadorDeReuniao.java
@@ -1,3 +1,13 @@
 public class MarcadorDeReuniao {
+    public void marcarReuniaoEntre(LocalDate dataInicial, LocalDate dataFinal, Collection<String> listaDeParticipantes) {
 
+    }
+
+    public void indicaDisponibilidadeDe(String participante, LocalDateTime inicio, LocalDateTime fim) {
+
+    }
+
+    public void mostraSobreposicao() {
+
+    }
 }
