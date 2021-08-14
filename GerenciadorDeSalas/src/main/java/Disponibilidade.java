a05c797477adaffe3bad59fb9bc8013bdc0cf5fa
João Paiva <joao.vitor.vbp@gmail.com>
2021-08-13 22:16:12 -0300

    Commit final (Espero)

--- GerenciadorDeSalas/src/main/java/Disponibilidade.java
+++ GerenciadorDeSalas/src/main/java/Disponibilidade.java
@@ -4,9 +4,6 @@ import java.util.List;
 
 public class Disponibilidade{//Alterei participante para Disponibilidade, removi a maioria das classes e metodos auxiliares ja que nunca foram usados nos testes.
   String email;
-  LocalDateTime inicio;
-  LocalDateTime fim;
-
   LocalDate inicioReuniao;
   LocalDate fimReuniao;
 
@@ -23,18 +20,5 @@ public class Disponibilidade{//Alterei participante para Disponibilidade, removi
     this.inicioReuniao = inicioReuniao;
     this.fimReuniao = fimReuniao;
   }
-  //Construtor padrão de Disponibilidade
 
-  //Método para adicionar horários (Nao cheguei a usar dentro do codigo, mas parece mais pratico)
-  public void adicionaHorario(LocalDateTime horarioInicial, LocalDateTime horarioFinal) {
-    dataLista.add(horarioInicial);
-    dataLista.add(horarioFinal);
-  }
-
-  //Método para exibir horários (Nao lembro se eh util)
-  public void exibirHorarios(){
-    for (int i = 0; i < dataLista.size(); i++) {
-      System.out.println(email+" "+i+" Elemento: "+dataLista.get(i));
-    }
-  }
 }
